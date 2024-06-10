88dda2ded1824cfdf21d64ffb592085a92da1349
jenisha-maharjan <sitashmagautam0@gmail.com>
2024-06-10 12:03:51 +0545

    JSfile

---
+++ myscript.js
@@ -0,0 +1,19 @@
+// <!-- variable is a container that store values 
+// variable name must be unique
+
+// at the beginning of variable name, avoid special character, number
+// we donot use js keywords and function name as variable name 
+// avoid space , avoid special character except underscore(_) while naming Variable
+
+// types 
+// let or var= value inside let or var is changable
+// const = value is constant i.e. not changable
+
+// for eg:
+// let x= 10
+// var x=10
+// const x=10
+
+// here x is variable name ,10  is value that is assigned to variable 'x'
+// =means assignment operator
+// // 
